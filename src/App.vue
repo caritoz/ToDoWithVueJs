0e4e28d434464a32e56f22481e25c3c4bc82d5b7
Carol <cgordillo.db@gmail.com>
2018-11-30 15:41:00 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" />
+    <h2>My ToDo</h2>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style lang="scss">
+@import "./variables.scss";
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+#app {
+  max-width: 400px;
+  margin: 0 auto;
+  line-height: 1.4;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $vue-blue;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
